07d99b9d548fed101383c0bd947e40793b520473
Oleksandr Tilha <oleksandr_tilha@epam.com>
2020-04-08 15:48:17 +0800

    module 3 - polishing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import logger from 'morgan';
 import router from './api/router';
 
 const app = express();
